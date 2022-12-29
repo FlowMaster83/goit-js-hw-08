b83f7927db78abb5806384942a71f65bcf96ba33
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2022-12-29 18:41:13 +0200

    check the task 08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+
+const videoPlayer = new Player(iframe);
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime) {
+    videoPlayer.setCurrentTime(savedTime);
+}
+
+videoPlayer.on(
+    'timeupdate',
+    throttle(function (data) {
+        const videoTime = data.seconds;
+
+        localStorage.setItem(STORAGE_KEY, videoTime);
+    }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const editedForm = {};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  editedForm[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(editedForm));
+}
+
+writeInForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(editedForm);
+}
+
+function writeInForm() {
+  const values = localStorage.getItem(LOCALSTORAGE_KEY);
+  let objectParse;
+  try {
+    objectParse = JSON.parse(values);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  if (values) {
+    for (const key in objectParse) {
+      let inputHTML = feedbackForm.querySelector(`[name="${key}"]`);
+      inputHTML.value = objectParse[key];
+    }
+  }
+}
